2d15e695bf6c0f43182e63b7abaf1aabf7cbb1ae
jasin1 <jasin.tairaidrissi@gmail.com>
2025-05-28 10:06:25 +0200

    add keen slider

---
+++ src/data/testimonials.js
@@ -0,0 +1,26 @@
+export const testimonialsData = [
+  {
+    name:"Shahram Sabet Zadeh",
+    date:"Oktober 14, 2023",
+    text:"Wail is een prettig, oprecht en eerlijk mens. Hij kan als trainingsacteur realistisch acteren, goed samenspel en hij is goed om anderen bewust te maken en hij creëert leermomenten. Hij geeft positieve en opbouwende feedback. Kortom een topper en ik werk heel graag met hem. Hij is een van de betere trainingsacteurs van Nederland.",
+    avatar:"",
+    link:"https://www.google.com/maps/contrib/114942458373236636233/photos/@52.1909763,5.2795551,519m/data=!3m2!1e3!4b1!4m3!8m2!3m1!1e1?hl=nl&entry=ttu&g_ep=EgoyMDI1MDUyMS4wIKXMDSoASAFQAw%3D%3D",
+    stars: 5
+  },
+  {
+    name:"Berendien",
+    date:"Oktober 14, 2023",
+    text:"Ik heb het genoegen gehad één keer met Waïl samen te werken en het smaakt echt naar meer! We dachten dat we baliemedewerkers zouden gaan trainen maar het bleek om leidinggevenden te gaan. Natuurlijk kun je dan niet dezelfde oefensituaties voor gebruiken. Het was fijn en heel verrassend te zien hoe gemakkelijk Waïl switchte naar andere casuïstiek. Geloofwaardig in zijn spel, aimabel in de samenwerking, een fijne sfeer opbouwend met cursisten en helder in de nabesprekingen. Kortom een allround acteur die je niet schrikt van verrassingen. Ik kan hem van harte aanbevelen!",
+    avatar:"",
+    link:"",
+    stars: 5
+  },
+  {
+    name:"Aris de Boer",
+    date:"Oktober 12, 2023",
+    text:"Wail is een van de meest oprechte, warme en intelligente trainingsacteurs waar ik mee heb mogen samenwerken. Ondanks zijn relatief jonge leeftijd heeft hij veel (levens)ervaring en kennis wat hij uitdraagt met een keurige vocabulaire. Daarnaast vind ik Wail een hele fijne vent met een aanstekelijke positieve energie waar ik altijd graag nog even mee blijf praten na een training i.p.v direct door naar huis te gaan.",
+    avatar:"",
+    link:"",
+    stars: 5
+  }
+]
