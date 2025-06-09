8d65c1aac2b59ac27edd258e2634f4b7080e9c71
jasin1 <jasin.tairaidrissi@gmail.com>
2025-06-09 16:22:44 +0200

    contentful blogposts

---
+++ src/lib/getAllBlogPosts.js
@@ -0,0 +1,19 @@
+import { deliveryClient } from "./contentful";
+
+export async function getAllBlogPosts(preview = false) {
+  // const client = preview ? previewClient : deliveryClient;
+
+  try {
+    const entries = await deliveryClient.getEntries({
+      content_type: "blogPost",
+      order: "-sys.createdAt",
+    });
+
+    console.log("Fetched from Contentful:", entries.items);
+
+    return entries.items;
+  } catch (error) {
+    console.error("fout bij ophalen van blogposts: ", error);
+    return [];
+  }
+}
