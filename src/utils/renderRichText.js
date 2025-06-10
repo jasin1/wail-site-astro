6ec0ed89c754e84c8a0813e7cf01e6fab443c268
jasin1 <jasin.tairaidrissi@gmail.com>
2025-06-10 17:16:48 +0200

    fix social shares

--- src/utils/renderRichText.js
+++ src/utils/renderRichText.js
@@ -1,11 +1,50 @@
 import { documentToHtmlString } from "@contentful/rich-text-html-renderer";
+import { BLOCKS, INLINES } from '@contentful/rich-text-types';
 
 export const renderOptions = {
-  renderText: (text) =>{
+  renderText: (text) => {
     return text.split('\n').join('<br />');
+  },
+  renderNode: {
+    [BLOCKS.EMBEDDED_ASSET]: (node) => {
+      const asset = node.data.target;
+      if (asset && asset.fields && asset.fields.file) {
+        const { file, title, description } = asset.fields;
+        const url = file.url.startsWith('//') ? `https:${file.url}` : file.url;
+        const alt = title || description || '';
+        return `<img src="${url}" alt="${alt}" class="embedded-image" />`;
+      }
+      return '';
+    },
+    [INLINES.EMBEDDED_ENTRY]: (node) => {
+      // Handle embedded entries if needed
+      return '';
+    },
+    [BLOCKS.EMBEDDED_ENTRY]: (node) => {
+      // Handle embedded entries if needed
+      return '';
+    }
   }
 };
 
-export function renderRichText(content){
+export function renderRichText(content) {
   return documentToHtmlString(content, renderOptions);
 }
+
+
+
+// import { documentToHtmlString } from "@contentful/rich-text-html-renderer";
+
+// export const renderOptions = {
+//   renderText: (text) =>{
+//     return text.split('\n').join('<br />');
+//   }
+// };
+
+// export function renderRichText(content){
+//   return documentToHtmlString(content, renderOptions);
+// }
+
+// export default function RichText({ content }) {
+//   return <div className="rich-text">{documentToReactComponents(content)}</div>;
+// }
